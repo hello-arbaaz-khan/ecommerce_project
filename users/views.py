306fff2be81d5183af1f09ab1f332836d4057100
hello-arbaaz-khan <hello.arbaaz.khan@gmail.com>
2025-10-28 14:27:31 +0500

    fix: signup POST route with csrf_exempt

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView,Response
 from django.contrib.auth import logout
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from rest_framework import status
 
 
 # Create your views here.
@@ -12,20 +13,25 @@ class SignupView(APIView):
     def post(self, request):
         serializer = SignupSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save() 
-            print("Signup API hit!")
-            return Response({'message': 'User created successfully'}, status=201)
-        return Response(serializer.errors, status=400)
+            serializer.save()
+            return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @method_decorator(csrf_exempt, name='dispatch')
 class LoginView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
-            return Response(serializer.validated_data, status=200)
-        return Response(serializer.errors, status=401)
+            data = serializer.validated_data
+            user = data['user']
+            return Response({
+                'message': f'Welcome {user.username}!',
+                'refresh': data['refresh'],
+                'access': data['access']
+            })
+        return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
 
 class LogoutView(APIView):
     def post(self, request):
         logout(request)
-        return Response({'message':'logged out successfully!'},status=200)
+        return Response({'message': 'logged out successfully!'}, status=status.HTTP_200_OK)
